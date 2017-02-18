46ad488bcb5e3b0dc4fff4798248ff3d7606a9fb
Aviel Aldama <teku-acceledrom-bass@live.com.mx>
2017-02-18 10:06:53 -0600

    Ejercicios basicos

---
+++ Ejercicio3.py
@@ -0,0 +1,9 @@
+"""Tipos basicos, listas, tuplas, diccionarios, cadenas
+   Apartado de listas"""
+LISTA1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+print LISTA1
+#Metodos para la lista append
+LISTA1.append(2)
+print LISTA1
+LISTA1.extend([1, 2, 3, 4])
+print LISTA1
---
+++ Ejercicio4.py
@@ -0,0 +1,5 @@
+"""Tipos basicos, listas, tuplas, diccionarios, cadenas
+   Apartado de tuplas"""
+TUPLA1 = (1, 2, 3, 4, 5, 6)
+print TUPLA1
+
